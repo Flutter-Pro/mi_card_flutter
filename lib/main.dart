5ba18f73313dda0851abfff4b45f542198f4235c
fritzgt <tuanis@live.com>
2020-09-16 15:31:31 +0300

    practice layouts

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,23 @@ void main() {
   runApp(MyApp());
 }
 
+//Use to enable hot reload
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.green,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            width: 100.0,
+            height: 100.0,
+            margin: EdgeInsets.all(20.0), //Outside the container border
+            padding: EdgeInsets.all(20.0), //Between border and child
+            color: Colors.white,
+            child: Text('Hello'),
+          ),
+        ),
       ),
     );
   }
